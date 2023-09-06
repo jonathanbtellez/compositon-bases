5b0dfc7c197fd79912e77821d8a5a2b79e8e02db
jonathanbtellez <jonathanbtellez@gmail.com>
2023-09-06 17:25:04 -0500

    reactive vs ref

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Reactive vs ref</h1>
+    <h3>{{ ironman }}</h3>
+    <h3>{{ hulk }}</h3>
+    <button @click="changeIronman">Iron man</button>
+    <button @click="changeHulk">Hulk</button>
+
+    <h3>ironman: {{ ironmanIsAlive }}</h3>
+    <h3>hulk: {{ hulkIsAlive }}</h3>
+    <button @click="changeStatus">Change state</button>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import { reactive, ref } from "vue";
 export default {
-  name: 'Home',
+  name: "Home",
+  setup() {
+    const ironman = ref({ name: "tony", edad: 50 });
+    const hulk = reactive({ name: "Bruce", edad: 50 });
+    const ironmanIsAlive = reactive(false);
+    const hulkIsAlive = ref(true);
+
+    const changeStatus = () => {
+      // ironmanIsAlive = !ironmanIsAlive;
+      hulkIsAlive.value = !hulkIsAlive.value;
+    };
+
+    return {
+      ironman,
+      hulk,
+      ironmanIsAlive,
+      hulkIsAlive,
+
+      changeIronman: () => {
+        ironman.value.name = "tony stark";
+        ironman.value.edad = "55";
+      },
+      changeHulk: () => {
+        hulk.name = "Bruce baner";
+        hulk.edad = "45";
+      },
+      changeStatus
+    };
+  },
 };
 </script>
